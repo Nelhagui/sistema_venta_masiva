ce8316bb083071ad645a7ce678a18b1ce13a2e2e
Nelson Aguiar <40721803+Nelhagui@users.noreply.github.com>
2023-12-31 13:24:01 -0300

    stock

--- resources/js/components/productos_stock/MainProductosActualizarStock.jsx
+++ resources/js/components/productos_stock/MainProductosActualizarStock.jsx
@@ -1,42 +1,16 @@
-import { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import TablaListaProductosActualizarStock from './TablaListaProductosActualizarStock';
+import TablaListProductosEnBase from './TablaListProductosEnBase';
+
 export default function MainProductosActualizarStock() {
-    const [productos, setProductos] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    useEffect(() => {
-        // Realizar la solicitud GET a la API de productos
-        fetch('/api/productos')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                // Actualizar el estado con la lista de productos
-                setProductos(data);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
     return (
         <>
-            {
-                isLoading
-                    ? "Cargando..."
-                    : <TablaListaProductosActualizarStock productos={productos} />
-            }
+            <TablaListProductosEnBase />
         </>
     )
 }
 
-if (document.getElementById('mainProductosActualizarStock')) {
-    const domNode = document.getElementById('mainProductosActualizarStock');
+if (document.getElementById('MainProductosActualizarStock')) {
+    const domNode = document.getElementById('MainProductosActualizarStock');
     const root = createRoot(domNode);
     root.render(<MainProductosActualizarStock />);
 }
