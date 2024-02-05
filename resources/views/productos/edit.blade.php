08bae701acd119249249c94e3c0a94714d20d831
Rosario Martinez <rosariomartinez927@yahoo.com>
2024-02-05 19:43:30 -0300

    edit productos

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -10,35 +10,66 @@
                 </div>
                 <form action="{{route('update.productos', $producto->id)}}" method="POST">
                     @csrf
-                    <label for="titulo">Titulo</label><br>
-                    <input type="text" name="titulo" required value="{{$producto->titulo}}"/><br>
-                    @error('titulo')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                    <div class="my-3">
+                        <label for="titulo">Título</label><br>
+                        <div style="width: 280px">
+                            <input type="text" name="titulo" required value="{{$producto->titulo}}"
+                            class="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        @error('titulo')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                    <label for="precio_costo">Precio costo</label><br>
-                    <input type="number" name="precio_costo" required value="{{$producto->precio_costo}}"/><br>
-                    @error('precio_costo')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                    <div class="my-3">
+                        <label for="precio_costo">Precio costo</label><br>
+                        <div style="width: 280px">
+                            <input type="number" name="precio_costo" required value="{{$producto->precio_costo}}"
+                            class="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        @error('precio_costo')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                    <label for="precio_venta">Precio venta</label><br>
-                    <input type="number" name='precio_venta' required value="{{$producto->precio_venta}}"/><br>
-                    @error('precio_venta')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                    <div class="my-3">
+                        <label for="precio_venta">Precio Venta</label><br>
+                        <div style="width: 280px">
+                            <input type="number" name="precio_venta" required value="{{$producto->precio_venta}}"
+                            class="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        @error('precio_venta')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                    <label for="stock">Stock</label><br>
-                    <input type="number" name='stock' required value="{{$producto->stock}}"/><br>
-                    @error('stock')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                    <div class="my-3">
+                        <label for="stock_actual">Stock</label><br>
+                        <div style="width: 280px">
+                            <input type="number" name="stock_actual" required value="{{$producto->stock_actual}}"
+                            class="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        @error('stock_actual')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                    <label for="codigo_barra">Codigo de barra</label><br>
-                    <input type="text" name='codigo_barra' value="{{$producto->codigo_barra}}"/><br>
-                    @error('codigo_barra')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                    <div class="my-3">
+                        <label for="codigo_barra">Código de barra</label><br>
+                        <div style="width: 280px">
+                            <input type="text" name="codigo_barra" required value="{{$producto->codigo_barra}}"
+                            class="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                        @error('codigo_barra')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
 
                     <button type="submit">Editar producto</button>
                 </form>
